dfcc275cd2e79eae8035d481bdda3f1521415729
pgj1394 <whrpwls1234@hanmail.net>
2025-05-21 17:57:38 +0000

    UploadPage style

--- frontend/react-echoweb/src/App.js
+++ frontend/react-echoweb/src/App.js
@@ -1,12 +1,13 @@
 // App.js
 import React from 'react';
-import UploadPage from './pages/UploadPage'; // 경로는 실제 위치에 맞게 조정
+import { BrowserRouter } from 'react-router-dom';
+import UploadPage from './pages/UploadPage';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <UploadPage />
-    </div>
+    </BrowserRouter>
   );
 }
 
