041f19791fa588278825e43f8c4a6d14624f76fc
pgj1394 <whrpwls1234@hanmail.net>
2025-05-21 17:32:26 +0000

    UploadPage

--- frontend/react-echoweb/src/App.js
+++ frontend/react-echoweb/src/App.js
@@ -1,20 +1,12 @@
-// src/App.js
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import LoginPage from './pages/LoginPage';
-import Signup_LiveValidationPage from './pages/Signup_LiveValidationPage';
-import SignupSuccessPage from './pages/SignupSuccessPage';
+import UploadPage from './pages/UploadPage'; // 경로는 실제 위치에 맞게 조정
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<Signup_LiveValidationPage />} />
-        <Route path="/signup-success" element={<SignupSuccessPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <UploadPage />
+    </div>
   );
 }
 
