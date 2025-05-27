d07325793d1d9770602e5b37a2229a68c6eac22e
pgj1394 <whrpwls1234@hanmail.net>
2025-05-27 08:09:33 +0000

    SegmentationPopup.js create

---
+++ frontend/react-echoweb/src/pages/SegmentationPopup.js
@@ -0,0 +1,25 @@
+// SegmentationProgressModal.jsx
+import React from 'react';
+import './SegmentationProgressModal.css';
+
+const SegmentationProgressModal = ({ progress }) => {
+  return (
+    <div className="seg-modal-overlay">
+      <div className="seg-modal-content">
+        <h1>Segmentation 진행중...</h1>
+        <p className="seg-desc">
+          segmentation이 진행중입니다. 시간이 소요될 수 있으니 잠시만 기다려 주십시오.
+        </p>
+        <div className="seg-progress-bar">
+          <div
+            className="seg-progress"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
+        <div className="seg-progress-label">{progress}%</div>
+      </div>
+    </div>
+  );
+};
+
+export default SegmentationProgressModal;
